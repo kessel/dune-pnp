e1f9b492b55a38a9abf82d2f7d143fdece9f8fc8
Stefan Kesselheim <stefan@kesselheim-online.de>
2012-07-03 12:12:17 +0200

    fixed wrapper script stuff

--- bin/dune_pnp.py
+++ bin/dune_pnp.py
@@ -7,7 +7,9 @@ solvers=["BCGS_SSORk", "BCGS_NOPREC", "CG_NOPREC", "CG_Jacobi", "CG_AMG_SSOR" ]
 
 from optparse import OptionParser
 
-parser = OptionParser()
+usage = """usage: %prog [options] [ configfile ]
+If no configuration file is given, only the binary corresponding to the given options is created."""
+parser = OptionParser(usage=usage)
 
 parser.add_option("-s", "--linearsolver", dest="linearsolver", default="BCGS_SSORk", 
                           help="choose linear solver", metavar="LINEARSOLVER")
@@ -26,14 +28,14 @@ for i in range(len(solvers)):
 
 progname=name+"_"+solvers[solverid-1]+"_"+str(options.pdegree)
 
-print "you want me to make ", progname
+print "If necessary, now we compile", progname
 
 pathname = os.path.dirname(sys.argv[0])        
-print 'full path =', os.path.abspath(pathname)
 oldpath=os.path.abspath(os.path.curdir)
 
 os.chdir(pathname+"/../src/")
-os.system("make "+progname)
+os.system("make "+progname+" 1>out 2>err")
+print "Done."
 os.chdir(oldpath)
 if len(args) > 0:
     os.system("mpirun -np "+str(options.np)+" " + pathname+"/../src/"+progname+" "+args[0])
--- src/pimp_my_makefile.py
+++ src/pimp_my_makefile.py
@@ -4,7 +4,7 @@ solvers=["BCGS_SSORk", "BCGS_NOPREC", "CG_NOPREC", "CG_Jacobi", "CG_AMG_SSOR" ]
 
 sys.stdout.write("EXTRA_PROGRAMS= ")
 for i in range(len(solvers)):
-    for j in range(3):
+    for j in range(1,4):
         sys.stdout.write("dune_pnp_"+solvers[i]+"_"+str(j)+ " ")
 
 sys.stdout.write("\n\n\n")
